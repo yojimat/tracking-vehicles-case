7a78a1b0655665cfd4a93189d45d42ee3a5362b1
Vinícius <visoccer22@hotmail.com>
2023-08-22 15:57:33 -0300

    Directions, Places

---
+++ nestjs-api/src/maps/maps.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MapsController } from './maps.controller';
+import { PlacesController } from './places/places.controller';
+import { PlacesService } from './places/places.service';
+import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
+
+@Module({
+  controllers: [MapsController, PlacesController, DirectionsController],
+  providers: [
+    PlacesService,
+    {
+      provide: GoogleMapsClient,
+      useValue: new GoogleMapsClient(),
+    },
+    DirectionsService,
+  ],
+})
+export class MapsModule {}
